efb922ce47d2813163bc18ef2e4a6185625f6ca4
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2025-02-12 20:00:59 -0700

    mostrar cargas en el mapa avance 1

--- src/views/Mapa/Mapa.tsx
+++ src/views/Mapa/Mapa.tsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { initMapa, parseCoordinates } from '@/lib/MapaService';
-import { mapContainerStyle, center } from '@/lib/MapaService';
-import { MenuBarMapa } from '@/components/components/MenuBarMapa';
-import { getPredios, getPrediosByDistance } from '@/lib/PredioService';
-import { Loader } from '@/components/components/Loader';
-import { getValvulas } from '@/lib/ValvulasService';
-import { ModalVerPredio } from '@/components/components/ModalVerPredio';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import { ModalImportarPrediosGeoJson } from '@/components/components/ModalImportarPrediosGeoJson';
+import React, { useEffect, useRef, useState } from "react";
+import { initMapa, parseCoordinates } from "@/lib/MapaService";
+import { mapContainerStyle, center } from "@/lib/MapaService";
+import { MenuBarMapa } from "@/components/components/MenuBarMapa";
+import {
+  getCargasTrabajoSinRelaciones,
+  getPredios,
+  getPrediosByDistance,
+} from "@/lib/PredioService";
+import { Loader } from "@/components/components/Loader";
+import { getValvulas } from "@/lib/ValvulasService";
+import { ModalVerPredio } from "@/components/components/ModalVerPredio";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { ModalImportarPrediosGeoJson } from "@/components/components/ModalImportarPrediosGeoJson";
+import { ComboBoxSplConceptos } from "@/components/components/ComboBoxSplConceptos";
 
 export const Mapa = () => {
+  const [cargasTrabajo, setCargasTrabajo] = useState([]);
+  const [loadingCargasTrabajo, setLoadingCargasTrabajo] = useState(false);
   const [predios, setPredios] = useState([]);
   const [valvulas, setValvulas] = useState([]);
   const [loadingPredios, setLoadingPredios] = useState(false);
@@ -19,16 +26,13 @@ export const Mapa = () => {
   const [selectedPredio, setSelectedPredio] = useState(null);
   const [map, setMap] = useState(null);
   const [polygons, setPolygons] = useState([]);
-  const [coordinates, setCoorinates] = useState
-    (
-      {
-        localizacion: {
-          latitude: 0,
-          longitude: 0,
-          distance: 500
-        }
-      }
-    )
+  const [coordinates, setCoorinates] = useState({
+    localizacion: {
+      latitude: 0,
+      longitude: 0,
+      distance: 500,
+    },
+  });
 
   useEffect(() => {
     init();
@@ -36,33 +40,60 @@ export const Mapa = () => {
 
   useEffect(() => {
     if (!loadingPredios) {
-      initMapa(predios, valvulas, setSelectedPredio, map, setMap, setCoorinates, setPolygons);
+      initMapa(
+        predios,
+        valvulas,
+        setSelectedPredio,
+        map,
+        setMap,
+        setCoorinates,
+        setPolygons
+      );
     }
-  }, [loadingPredios])
+  }, [loadingPredios]);
 
   const init = async () => {
     await getPrediosByDistance(setLoadingPredios, setPredios, coordinates);
     // await getValvulas(setLoadingPredios, setValvulas);
     setLoaded(true);
-  }
+  };
 
   useEffect(() => {
     if (selectedPredio != null) {
       triggerModalRef?.current?.click();
     }
-  }, [selectedPredio])
+  }, [selectedPredio]);
+
+  useEffect(() => {
+    getCargasTrabajoSinRelaciones(setLoadingCargasTrabajo, setCargasTrabajo);
+  }, []);
 
   return (
     <div>
       <p>Mapa Predios</p>
       <div className="mb-4">
-        <Card className='mt-3'>
+        <Card className="mt-3 flex items-center gap-2 border-none">
           <ModalImportarPrediosGeoJson />
-          <Button onClick={() => {
-            polygons.forEach(polygon => polygon.setMap(null));
-          }}>Limpiar</Button>
+          <Button
+            onClick={() => {
+              polygons.forEach((polygon) => polygon.setMap(null));
+            }}
+          >
+            Limpiar
+          </Button>
+          <ComboBoxSplConceptos
+            className={"w-[500px] z-50"}
+            placeholder={"Buscar carga de trabajo ..."}
+            loading={loadingCargasTrabajo}
+            items={cargasTrabajo}
+            accessName={"nombre_carga"}
+          />
         </Card>
-        <ModalVerPredio predioId={selectedPredio} setPredioId={setSelectedPredio} trigger={<Button ref={triggerModalRef} className='hidden'></Button>} />
+        <ModalVerPredio
+          predioId={selectedPredio}
+          setPredioId={setSelectedPredio}
+          trigger={<Button ref={triggerModalRef} className="hidden"></Button>}
+        />
       </div>
       {loadingPredios && (
         <div className="absolute z-20">
